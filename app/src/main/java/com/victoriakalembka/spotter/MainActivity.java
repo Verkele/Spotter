955ee73441ac8a97e755796a554dfe0859c1ac45
Verkele <verkele@gmail.com>
2016-12-18 12:49:50 -0500

    First commit for Spotter

---
+++ app/src/main/java/com/victoriakalembka/spotter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.victoriakalembka.spotter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//home screen
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
